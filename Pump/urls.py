f3ccbbefdb921c3e49dbba6caa552de2501cff3d
Yaroslav Biziuk <131271125+eLQeR@users.noreply.github.com>
2024-12-29 17:10:54 +0200

    add 3d

--- Pump/urls.py
+++ Pump/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('3D', views.index_3D, name='index_3D'),
     path('pumps/', views.pump_list, name='pump_list'),
     path('pumps/<int:pk>/', views.pump_detail, name='pump_detail'),
     path('pumps/<int:pk>/control/', views.pump_control, name='pump_control'),
